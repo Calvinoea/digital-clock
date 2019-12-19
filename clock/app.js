978c91b8de011c3421cefc45efda60925dd6f220
Calvinoea <39657008+Calvinoea@users.noreply.github.com>
2019-12-19 17:02:49 +0100

    Add files via upload

---
+++ clock/app.js
@@ -0,0 +1,37 @@
+function time() {
+
+    let fullDate = new Date();
+    let hours = fullDate.getHours();
+    let minutes = fullDate.getMinutes();
+    let seconds = fullDate.getSeconds();
+
+
+    const hours_div = document.querySelector("#hour");
+    const minutes_div = document.querySelector("#minute");
+    const seconds_div = document.querySelector("#second");
+
+    if (hours < 10) {
+
+        hours = '0' + hours;
+
+    }
+
+    if (minutes < 10) {
+
+        minutes = `0${minutes}`
+
+    }
+
+    if (seconds < 10) {
+
+        seconds = `0${seconds}`;
+
+    }
+
+    hours_div.innerHTML = `${hours}:`;
+    minutes_div.innerHTML = `${minutes}: `;
+    seconds_div.innerHTML = `${seconds} `;
+
+}
+
+setInterval(time, 500)
